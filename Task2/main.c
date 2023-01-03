3d81abbf5e2f271dcf6aa8069893acfad59c4638
Dmytro Tupkalenko <119056161+tupkalenkodi@users.noreply.github.com>
2023-01-03 22:47:55 +0100

    Final commit Task2

--- Task2/main.c
+++ Task2/main.c
@@ -3,7 +3,6 @@
 
 //Function that returns length of the word
 int length_calculate(char word[]){
-
     int i = 0;
     while (word[i] != ';'){
         i++;
@@ -23,16 +22,26 @@ int check_if_words_are_equal(char word1[], char word2[]){
 
 
 int main (){
-    //Hidden_word will be in format "....;"(With the semicolumn at the end).
+    printf("You need to guess the word, moreover the length of it is given, however, you have 6 tries. \nIf you don't guess the letter the number of tries will decrease by 1, otherwise, the number of tries will stay the same.\n");
+    //Hidden_word will be in format "....;"(With the semicolon at the end).
     char hidden_word[20];
 
-    //Assigning random value to hidden_word.
-    char array_of_random_words[][20] = {"extreme;", "clarify;", "ton;", "compartment;", "rung;", "grimace;", "freshman;","command;", "ghostwriter;", "core;", "rack;", "beneficiary;", "hot;", "adoption;", "fun;", "myth;"};
+    //Choosing some random word.
+    char array_of_random_words[15][20] = {"extreme;", "clarify;", "ton;", "compartment;", "rung;", "grimace;", "freshman;","command;", "ghostwriter;", "core;", "rack;", "beneficiary;", "hot;", "adoption;", "fun"};
     int random_number;
     printf("Choose some random number from 1 to 15:\n");
     scanf("%i", &random_number);
+    if (random_number != 1 && random_number != 2 && random_number != 3 && random_number != 4 && random_number != 5 && random_number != 6 && random_number != 7 && random_number != 8 && random_number != 9 && random_number != 10 && random_number != 11 && random_number != 12 && random_number != 13 && random_number != 14 && random_number != 15){
+        printf("\n");
+        printf("Provide me with correct number.\n");
+        scanf("%i", &random_number);
+        printf("\n");
+    }
 
-    printf("%s", array_of_random_words[random_number - 1]);
+    //Assigning this word to hidden_word.
+    for (int j = 0; j <= length_calculate(array_of_random_words[random_number - 1]); j++) {
+        hidden_word[j] = array_of_random_words[random_number - 1][j];
+    }
 
     //Variable that represent length of hidden_word.
     int size_of_hidden_word = length_calculate(hidden_word);
@@ -41,53 +50,55 @@ int main (){
     char guessed_word[size_of_hidden_word];
 
     //Filling guessed_word with underscores.
+    int counter = 0;
     for (int i = 0; i < size_of_hidden_word; i++){
         guessed_word[i] = '_';
-        //printf("%c", guessed_word[i]);
-    }
-
-    //Number that corresponds quantity of possible guesses.
-    int left_parts_of_figure = 6;
-
-    //Printing the guessed word.
-    for (int i = 0; i < size_of_hidden_word; i++){
         printf("%c", guessed_word[i]);
+        counter++;
     }
+    guessed_word[counter] = ';';
     printf("\n");
 
+    //Number that corresponds quantity of tries left.
+    int left_parts_of_figure = 6;
+
     int result_of_comparison;
-    
     while (left_parts_of_figure != 0){
         //If user won.
         result_of_comparison = check_if_words_are_equal(hidden_word, guessed_word);
         if (result_of_comparison == 1){
-            printf("\n");
             printf("Congratulations, you won!");
+            printf("\n");
             return 0;
         }
         else {
-            //Ask for the letter and position.
-            int position = 0;
+            //Ask for the letter.
             char users_letter;
-
-            printf("Type me the position of the letter(Starting with 1):\n");
-            scanf("%i", &position);
             getchar();
             printf("Type me the letter:\n");
             scanf("%c", &users_letter);
-            getchar();
 
-            //Now we have two possibilities.
-
-            //If user guessed.
-            if (hidden_word[position - 1] == users_letter) {
-                guessed_word[position - 1] = users_letter;
+            //Assign one additional variable, for case when user doesn't guess.
+            char guessed_word_0[20];
+            for (int n = 0; n <= length_calculate(hidden_word); n++) {
+                guessed_word_0[n] = guessed_word[n];
             }
 
-                //If user did not guess.
-            else if (hidden_word[position - 1] != users_letter) {
+            //Now I check if user's letter contained in hidden_word.
+            for (int k = 0; k <= length_calculate(hidden_word); k++) {
+                //If user guessed.
+                if (hidden_word[k] == users_letter) {
+                    guessed_word[k] = users_letter;
+                }
+            }
+
+            //If user haven't guessed.
+            int comparison_result = check_if_words_are_equal(guessed_word_0, guessed_word);
+            if (comparison_result == 1){
                 left_parts_of_figure--;
             }
+
+            //Print the modified word.
             printf("\n");
             for (int i = 0; i < size_of_hidden_word; i++) {
                 printf("%c", guessed_word[i]);
